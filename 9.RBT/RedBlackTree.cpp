cc989b1c7441f412990d59a8a82551e56b5ac1ce
ronniej <ronniej@naver.com>
2025-02-18 17:50:41 +0900

    레드 블랙 트리 구현 완료

--- 9.RBT/RedBlackTree.cpp
+++ 9.RBT/RedBlackTree.cpp
@@ -24,7 +24,8 @@ RedBlackTree::RedBlackTree()
 
 RedBlackTree::~RedBlackTree()
 {
-	// Todo: 모든 노드 삭제.
+	DestroyRecursive(root);
+	SafeDelete(nil);
 }
 
 bool RedBlackTree::Find(int data, Node** outNode)
@@ -53,7 +54,29 @@ bool RedBlackTree::Insert(int data)
 	}
 
 	// 노드 생성 후 결과 반환.
-	return Insert(CreateNode(data, Color::Red));
+	Insert(CreateNode(data, Color::Red));
+	return true;
+}
+
+void RedBlackTree::Remove(int data)
+{
+	// 삭제할 노드 검색에 실패하면 삭제 실패.
+	Node* deleted = nullptr;
+	if (!Find(data, &deleted))
+	{
+		std::cout 
+			<< "오류: 노드 삭제 실패. 삭제할 노드를 찾지 못했습니다. (검색 값: "
+			<< data << ")\n";
+		return;
+	}
+
+	// 삭제 진행.
+	RemoveImpl(deleted);
+}
+
+void RedBlackTree::Print(int depth, int blackCount)
+{
+	PrintRecursive(root, depth, blackCount);
 }
 
 bool RedBlackTree::FindRecursive(Node* node, Node** outNode, int data)
@@ -219,6 +242,137 @@ void RedBlackTree::RestructureAfterInsert(Node* newNode)
 	root->SetColor(Color::Black);
 }
 
+void RedBlackTree::RestructureAfterRemove(Node* node)
+{
+	// 더블 블랙 문제 해결.
+	while (node->Parent() != nullptr && node->GetColor() == Color::Black)
+	{
+		// 형제 노드 구하기.
+		if (node == node->Parent()->Left())
+		{
+			Node* sibling = node->Parent()->Right();
+			if (sibling == nil)
+			{
+				break;
+			}
+
+			// Case1: 형제 노드가 빨간색.
+			// 해결: 형제 노드를 Black으로 변경하고, 
+			// 부모를 Red로 바꾼 후 부모 기준으로 좌회전 또는 우회전.
+			if (sibling->GetColor() == Color::Red)
+			{
+				sibling->SetColor(Color::Black);
+				node->Parent()->SetColor(Color::Red);
+
+				// 좌회전.
+				RotateToLeft(node->Parent());
+				continue;
+			}
+
+			// Case2: 형제 노드가 검정색일 때.
+			// 해결: 형제 노드를 Red로 변경하여 Black Height를 줄이고, 
+			// 부모를 새로운 노드로 설정 후 다시 검사.
+			if (sibling->Left()->GetColor() == Color::Black
+				&& sibling->Right()->GetColor() == Color::Black)
+			{
+				sibling->SetColor(Color::Red);
+				node = node->Parent();
+				continue;
+			}
+
+			// Case3: 형제 노드 검정. 형제 노드의 왼쪽 자손이 빨간색.
+			// 해결: 형제의 왼쪽 자식을 Black으로 변경, 
+			// 형제를 Red로 변경 후 형제 기준 우회전.
+			if (sibling->Left()->GetColor() == Color::Red)
+			{
+				sibling->Left()->SetColor(Color::Black);
+				sibling->SetColor(Color::Red);
+
+				// 우회전.
+				RotateToRight(sibling);
+
+				// 회전 후 형제 위치 업데이트.
+				sibling = node->Parent()->Right();
+			}
+
+			// Case4: 형제 노드는 Black, 형제의 오른쪽 자식이 Red.
+			// 해결: 형제를 부모와 같은 색으로 설정, 
+			// 부모를 Black으로 변경, 
+			// 형제의 오른쪽 자식을 Black으로 변경 후 부모 기준 좌회전.
+			if (sibling->Right()->GetColor() == Color::Red)
+			{
+				sibling->SetColor(sibling->Parent()->GetColor());
+				sibling->Parent()->SetColor(Color::Black);
+				sibling->Right()->SetColor(Color::Black);
+				RotateToLeft(node->Parent());
+			}
+
+			continue;
+		}
+
+		// 형제 노드 구하기.
+		Node* sibling = node->Parent()->Left();
+		if (sibling == nil)
+		{
+			break;
+		}
+
+		// Case1: 형제 노드가 빨간색.
+		// 해결: 형제 노드를 Black으로 변경하고, 
+		// 부모를 Red로 바꾼 후 부모 기준으로 좌회전 또는 우회전.
+		if (sibling->GetColor() == Color::Red)
+		{
+			sibling->SetColor(Color::Black);
+			node->Parent()->SetColor(Color::Red);
+
+			// 좌회전.
+			RotateToRight(node->Parent());
+			continue;
+		}
+
+		// Case2: 형제 노드가 검정색일 때.
+		// 해결: 형제 노드를 Red로 변경하여 Black Height를 줄이고, 
+		// 부모를 새로운 노드로 설정 후 다시 검사.
+		if (sibling->Left()->GetColor() == Color::Black
+			&& sibling->Right()->GetColor() == Color::Black)
+		{
+			sibling->SetColor(Color::Red);
+			node = node->Parent();
+			continue;
+		}
+
+		// Case3: 형제 노드 검정. 형제 노드의 오른쪽 자손이 빨간색.
+		// 해결: 형제의 오른쪽 자식을 Black으로 변경, 
+		// 형제를 Red로 변경 후 형제 기준 좌회전.
+		if (sibling->Right()->GetColor() == Color::Red)
+		{
+			sibling->Right()->SetColor(Color::Black);
+			sibling->SetColor(Color::Red);
+
+			// 우회전.
+			RotateToLeft(sibling);
+
+			// 회전 후 형제 위치 업데이트.
+			sibling = node->Parent()->Left();
+		}
+
+		// Case4: 형제 노드는 Black, 형제의 왼쪽 자식이 Red.
+		// 해결: 형제를 부모와 같은 색으로 설정, 
+		// 부모를 Black으로 변경, 
+		// 형제의 왼쪽 자식을 Black으로 변경 후 부모 기준 우회전.
+		if (sibling->Left()->GetColor() == Color::Red)
+		{
+			sibling->SetColor(sibling->Parent()->GetColor());
+			sibling->Parent()->SetColor(Color::Black);
+			sibling->Left()->SetColor(Color::Black);
+			RotateToRight(node->Parent());
+		}
+	}
+
+	// 루트 노드 블랙.
+	root->SetColor(Color::Black);
+}
+
 void RedBlackTree::RotateToLeft(Node* node)
 {
 	// 오른쪽 자식 노드.
@@ -301,4 +455,216 @@ void RedBlackTree::RotateToRight(Node* node)
 	// 우회전 마무리.
 	left->SetRight(node);
 	node->SetParent(left);
+}
+
+Node* RedBlackTree::FindMinRecursive(Node* node)
+{
+	// 탈출 조건.
+	if (node == nil)
+	{
+		return nullptr;
+	}
+
+	// 왼쪽 하위 노드가 더 이상 없으면 현재 노드 반환.
+	if (node->Left() == nil)
+	{
+		return node;
+	}
+
+	return FindMinRecursive(node->Left());
+}
+
+Node* RedBlackTree::FindMaxRecursive(Node* node)
+{
+	// 탈출 조건.
+	if (node == nil)
+	{
+		return nullptr;
+	}
+
+	// 오른쪽 하위 노드가 더 이상 없으면 현재 노드 반환.
+	if (node->Right() == nil)
+	{
+		return node;
+	}
+
+	return FindMaxRecursive(node->Right());
+}
+
+void RedBlackTree::RemoveImpl(Node* node)
+{
+	// 삭제 대상 노드.
+	Node* removed = nullptr;
+
+	// 삭제할 위치의 대체 노드.
+	Node* trail = nil;
+
+	Node* target = node;
+
+	// 자손이 하나 이하인 경우.
+	if (target->Left() == nil || target->Right() == nil)
+	{
+		removed = target;
+
+		// 자손이 하나만 있을 때.
+		if (node->Left() != nil && node->Right() == nil)
+		{
+			trail = node->Left();
+		}
+		else if (node->Right() != nil && node->Left() == nil)
+		{
+			trail = node->Right();
+		}
+	}
+
+	// 자손이 모두 있는 경우.
+	else
+	{
+		// 대체할 노드 검색.
+		// (왼쪽 하위 트리에서 가장 큰값을 대체 노드로 설정).
+		removed = FindMaxRecursive(target->Left());
+
+		// 대체 노드가 존재하면 해당 데이터 설정.
+		//if (removed != nullptr && removed != nil)
+		//{
+		//	// 현재 노드의 값을 대상 노드의 값으로 변경.
+		//	target->SetData(removed->Data());
+		//}
+
+		// 현재 노드의 값을 대상 노드의 값으로 변경.
+		target->SetData(removed->Data());
+	}
+
+	// 대상 노드가 있는 경우.
+	if (trail != nil /*&& trail != nullptr*/)
+	{
+		trail->SetParent(removed->Parent());
+	}
+
+	// removed 노드가 root인 경우.
+	if (removed->Parent() == nullptr)
+	{
+		root = trail;
+	}
+	// root가 아닐 때.
+	else
+	{
+		// trail 노드를 removed의 원래 위치로 설정.
+		if (removed == removed->Parent()->Left())
+		{
+			removed->Parent()->SetLeft(trail);
+		}
+		else
+		{
+			removed->Parent()->SetRight(trail);
+		}
+	}
+
+	// 재정렬 여부 확인후 진행.
+	if (removed->GetColor() == Color::Black && trail != nil)
+	{
+		// 재정렬 진행.
+		RestructureAfterRemove(trail);
+	}
+
+	// 노드 삭제.
+	SafeDelete(removed);
+}
+
+void RedBlackTree::DestroyRecursive(Node* node)
+{
+	// 재귀 탈출 조건.
+	if (node == nil)
+	{
+		return;
+	}
+
+	// 자식이 있는 경우.
+	DestroyRecursive(node->Left());
+	DestroyRecursive(node->Right());
+
+	// 노드 삭제.
+	SafeDelete(node);
+}
+
+void RedBlackTree::PrintRecursive(Node* node, int depth, int blackCount)
+{
+	// 탈출 조건.
+	if (node == nil)
+	{
+		return;
+	}
+
+	// 노드 색상이 검정이면 blackCount 증가.
+	if (node->GetColor() == Color::Black)
+	{
+		++blackCount;
+	}
+
+	// 부모 표기 문자열 설정.
+	int parentData = 0;
+	const char* position = "Root";
+
+	// 부모 노드가 있는지 확인.
+	if (node->Parent())
+	{
+		// 부모 노드의 데이터 저장.
+		parentData = node->Parent()->Data();
+
+		// 부모 노드로부터 현재 노드의 위치 설정.
+		if (node == node->Parent()->Left())
+		{
+			position = "Left";
+		}
+		else
+		{
+			position = "Right";
+		}
+	}
+
+	// 검은색 수 출력을 위한 문자열.
+	char blackCountString[50] = { };
+
+	// 자손이 없으면 현재까지의 검은색 노드 수 설정.
+	if (node->Left() == nil && node->Right() == nil)
+	{
+		sprintf_s(blackCountString, "------ %d", blackCount);
+	}
+
+	// Depth 출력.
+	for (int ix = 0; ix < depth; ++ix)
+	{
+		std::cout << "  ";
+	}
+
+	// 노드 색상에 따른 콘솔 설정.
+	if (node->GetColor() == Color::Red)
+	{
+		SetTextColor(TextColor::Red);
+	}
+	else
+	{
+		SetTextColor(TextColor::White);
+	}
+
+	// 현재 노드 값 출력.
+	std::cout
+		<< node->Data() << " "
+		<< node->ColorString() << " ["
+		<< position << ", "
+		<< parentData << "] " 
+		<< blackCountString << "\n";
+
+	// 노드를 출력한 뒤에는 콘솔 원래대로.
+	SetTextColor(TextColor::White);
+
+	// 하위 노드 출력.
+	PrintRecursive(node->Left(), depth + 1, blackCount);
+	PrintRecursive(node->Right(), depth + 1, blackCount);
+}
+
+void SetTextColor(TextColor color)
+{
+	static HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(console, (int)color);
 }
